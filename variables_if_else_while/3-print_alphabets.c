342e8760a00952074a30de981ca86c857f18fde3
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-23 15:07:41 +0200

    12th task

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 97;
+
+	while (c < 123)
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 65;
+	while (c < 91)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+
+	return (0);
+}		
