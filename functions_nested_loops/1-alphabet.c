53ca4c8c5c056f11d13f5914c1ce52178be0ce67
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-25 11:29:36 +0200

    1th task

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 97;
+
+	while (c < 123)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
