498afc52550e87a0c11d7f959392772474746242
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-28 16:04:11 +0100

    3th task

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_isdigit - checks if its a digit and return 1 if it is one.
+ *@c: its the variable that is gonna be checked by isdigit.
+ * Return: Always 0.
+ */
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
