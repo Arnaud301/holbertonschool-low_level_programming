faca2bceadbfd383242fb4126e74edc29d96294a
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-29 15:41:37 +0100

    7th task

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
-print_diagonal - Entry point
-@n : p
+*print_diagonal - prints diagonals * n
+@n : idk
 Return: void
 */
 
 void print_diagonal(int n)
 {
-    int a;
-    int b;
+	int a;
+	int b;
 
-    if (n > 0)
-    {
-        for (a = 0; a < n; a++)
-        {
-            for (b = 0; b < a; b++)
-                _putchar(' ');
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			_putchar(' ');
 
-            _putchar('\\');
+			_putchar('\\');
 
-            if (a == (n - 1))
-                continue;
-            _putchar('\n');
-        }
-    }
-    _putchar('\n');
+			if (a == (n - 1))
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
 }
 
