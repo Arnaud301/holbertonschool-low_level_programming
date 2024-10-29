e442822ba16176809ebf979ddbdc56d367527fc5
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-29 12:07:06 +0100

    6th task

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_line - print n number of lines.
+ *@n: number of lines.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+}
