dd383df5a4afbaf989f2b47dbc99ce104af63b61
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-29 12:23:31 +0100

    7th task

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_line - print n number of lines.
+ *@n: number of lines.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar('\\');
+			n--;
+		}
+
+		_putchar('\n');
+	}
+}
