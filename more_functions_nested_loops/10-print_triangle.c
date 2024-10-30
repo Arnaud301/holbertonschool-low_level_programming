342a053c1699f5bb93ab64385eaf38dce5eaa78a
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-30 11:44:48 +0100

    10th task

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,9 +16,9 @@ void print_triangle(int size)
 		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < a; b++)
-			_putchar('\n');
-
 			_putchar('#');
+
+			_putchar('\n');
 
 			if (a == (size - 1))
 				continue;
