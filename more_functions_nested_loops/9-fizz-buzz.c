a8475851f044902f1b78a46f3d0f72473c1a4ffb
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-30 10:47:19 +0100

    9th task

---
+++ more_functions_nested_loops/9-fizz-buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 100; c++)
+	{
+		if (c % 3 == 0 && c % 5 != 0)
+		{
+			printf("Fizz");
+			printf(" ");
+			continue;
+		}
+		else if (c % 5 == 0  && c % 3 != 0)
+		{
+			printf("Buzz");
+			printf(" ");
+			continue;
+		}
+		else if (c % 3 == 0 && c % 5 != 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+			continue;
+		}
+		printf(c);
+		printf(" ");
+	}
+}
---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 100; c++)
+	{
+		if (c % 3 == 0 && c % 5 != 0)
+		{
+			printf("Fizz");
+			printf(" ");
+			continue;
+		}
+		else if (c % 5 == 0  && c % 3 != 0)
+		{
+			printf("Buzz");
+			printf(" ");
+			continue;
+		}
+		else if (c % 3 == 0 && c % 5 != 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+			continue;
+		}
+		printf("%d", c);
+		printf(" ");
+	}
+	return (0);
+}
