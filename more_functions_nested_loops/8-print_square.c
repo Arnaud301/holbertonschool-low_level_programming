ffcda92753601e3e573b50976cd0d2c42f376f3b
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-30 09:44:16 +0100

    8th task

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,32 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *print_square - prints size
- *@size: size of the cube
- * Return: Always 0.
- */
+* print_square - Entry point
+* @size: p
+* Return: void
+*/
+
 void print_square(int size)
 {
-	int n = 0 + size;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (n != 0)
-		{
-			while (size > 0)
-			{
-				_putchar('#');
-				size--;
-			}
-			_putchar('\n');
-			n--;
-		}
+    int b;
+    int a;
 
-		_putchar('\n');
-	}
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+
+    for (a = 0; a < size; a++)
+    {
+        for (b = 0; b < size; b++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+
 }
+
