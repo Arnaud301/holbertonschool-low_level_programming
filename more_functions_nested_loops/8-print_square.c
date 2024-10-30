fc237413f509a98d22c3ec84a49be1413b837d6e
Arnaud301 <arnaud2bonnet@gmail.com>
2024-10-30 09:50:45 +0100

    8th task

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,22 +9,22 @@
 void print_square(int size)
 {
 
-    int b;
-    int a;
+	int b;
+	int a;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
-    for (a = 0; a < size; a++)
-    {
-        for (b = 0; b < size; b++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 
 }
 
