ed8b4153e4af7e2919f259a33ad5d2a8d17338f2
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-03 23:28:25 +0100

    3th task

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - idk
+ * @s1: idk
+ * @s2: idk
+ * Return: idk
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i] && s1[i] == '\0')
+		i++;
+
+	return (s1[i] - s2[i]);
+}
