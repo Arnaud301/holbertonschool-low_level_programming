6470eeb80f16268ea245269a2628581cc919818c
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-03 19:54:17 +0100

    10th task

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * ZDSQDZQDQDSDQE
+ * _atoi - idk
+ * @s: idk
+ * Return: idk
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, neg = 0;
+	unsigned int res = 0;
+
+	while ((s[i] >= 0 && s[i] <= 47) || (s[i] >= 58 && s[i] < 127))
+	{
+		if (s[i] == '-')
+			neg = (neg * -1);
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	return (res * neg);
+}
