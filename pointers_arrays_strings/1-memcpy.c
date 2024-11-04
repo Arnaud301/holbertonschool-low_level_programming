4af9ebfbd11296c324125ad8c34be82dcb59d3a3
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-04 13:52:27 +0100

    1th task

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - idk
+ * @s: idk
+ * @b: idk
+ * @n: idk
+ * Return: Always 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[0];
+		if (src[i] != '\0')
+			i++;
+		else
+			break;
+	}
+
+	return (dest);
+}
