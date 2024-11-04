9efd9f5b21663a27bf48fd1410e5af1bb8a5c8ec
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-04 01:42:59 +0100

    7th task

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - idk
+ * @s: idk
+ *
+ * Return: Return smth
+ */
+
+char *leet(char *s)
+{
+	char letters[] = {'a', 'e', 'o', 't', 'l'};
+	char numbers[] = {'4', '3', '0', '7', '1'};
+	int length = 0, index;
+
+	while (s[length] != '\0')
+	{
+		for (index = 0; numbers[index] != '\0'; index++)
+		{
+			if (s[length] == letters[index] || s[length] == letters[index] - 32)
+				s[length] = numbers[index];
+		}
+		length++;
+	}
+	return (s);
+}
