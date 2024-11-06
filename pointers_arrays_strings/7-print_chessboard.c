e50384a2e7c283f8f4ebdba079e70354a9ce2ddc
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-06 01:15:13 +0100

    7th task

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		putchar('\n')
+	}
+}
