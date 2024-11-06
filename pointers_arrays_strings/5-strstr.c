4bffa11378222220adfbd29a46d2e1b8baf9f705
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-06 01:09:22 +0100

    5th task

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,8 +14,8 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i] != 0)
 	{
-		while (haystack[i+j] == needle[j] && needle[j] != '\0')
-		{	
+		while (haystack[i + j] == needle[j] && needle[j] != '\0')
+		{
 			j++;
 		}
 		if (needle[j] == '\0')
