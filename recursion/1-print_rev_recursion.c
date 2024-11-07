25be2a998ad7628cff9c92ce7e2b1d821f3c972c
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-07 15:09:25 +0100

    1th task

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	int end = 0;
+	int count = 1;
+
+	if (s[end] != '\0')
+	{
+		end++;
+		_print_rev_recursion(s);
+	}
+	if (end >= 0)
+	{
+		if (end == '\0')
+		{
+			end - count;
+		}
+		_putchar(s[end]);
+		end--;
+		count++;
+		_print_rev_recursion(s);
+	}
+	else
+		_putchar('\n')
+}
