5af7323e3f4fe4baa44bf369871e846b0773e1f3
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-14 00:09:58 +0100

    3th task

---
+++ malloc_free/3-alloc_girl.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid - function
+ *@width: int width
+ *@height: height int
+ *Return: return
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **array;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int *) * height);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+
+		if (matrix[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(array[j]);
+
+			free(array);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	return (array);
+}
