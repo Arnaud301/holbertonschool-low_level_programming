aea00b9d9c112df86d90f93346c0e83fd39cb877
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-21 11:02:12 +0100

    0th task

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *print_name - prints
+ *@name: name of the person
+ *Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+
+}
