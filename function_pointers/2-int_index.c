e70050f666beca4d612deda7a4334c7a1ca4b700
Arnaud301 <arnaud2bonnet@gmail.com>
2024-11-22 14:07:22 +0100

    2th task

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - function
+ *@array: array
+ *@size: the size of array
+ *@cmp: pointer
+ *Return: return
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || cmp == NULL || array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		i++;
+
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
