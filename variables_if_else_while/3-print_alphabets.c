e0ddf4cf38432bda53dee818af47328487c8f0de
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-19 14:15:17 +0100

    3th task

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 on sucess
+ */
+int main(void)
+{
+	int a;
+	int A;
+
+	for (a = 97; a < 123; a++)
+		putchar(a);
+	for (A = 65; a < 90; A++)
+		putchar(A);
+	putchar('\n');
+	return (0);
+}
