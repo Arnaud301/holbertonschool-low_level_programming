19c2737a81b70a02e9f83e05f5b3aa46fc1c2707
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-21 14:07:32 +0100

    0th task

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0 on sucess
+ */
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar('\n');
+	return (0);
+}
