3dfbe4988461885dc96408f6a5be766ca5c431c7
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-24 09:09:21 +0100

    0th task correction

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *_isalpha - function
+ *_isupper - function
  *@c: int
  *Return: 0 on sucess else 1
  */
-int _isalpha(int c)
+int _isupper(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
