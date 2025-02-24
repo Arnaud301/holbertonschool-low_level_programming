c3a3a56f7df7def0183f35e5e3646655b5514f0a
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-24 10:44:58 +0100

    4th task

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_most_numbers - function
+ *Return: 0 on sucess
+ */
+void print_most_numbers(void);
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{	
+		if (a == 50 | a == 52)
+			continue;
+		_putchar(a);
+	}
+	_putchar('\n');
+}
