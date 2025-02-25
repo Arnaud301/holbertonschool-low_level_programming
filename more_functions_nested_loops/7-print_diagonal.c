1188e2f596792b7d98440bb98ad0cce80b9a041c
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-25 14:39:42 +0100

    7th task

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(' ');
+		if (a == n)
+			_putchar("\\");
+	}
+	_putchar('\n');
+}
