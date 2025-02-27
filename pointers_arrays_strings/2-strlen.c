0e5ad3ba7bdb24889d68e16ec441cb8744faa7b0
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-27 14:17:29 +0100

    2th task

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,11 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	if (s[i] == NULL)
+	if (*s[i] == NULL)
 		return (0);
 	else
 	{
-		while (s[i] != NULL)
+		while (*s[i] != NULL)
 			i++;
 	}
 	return (i);
