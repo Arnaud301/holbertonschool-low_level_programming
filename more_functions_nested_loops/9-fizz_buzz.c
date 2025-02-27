a9224909fb5c6aaaee162f7a10ff8f4b9f979956
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-27 09:09:04 +0100

    9th task

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,17 @@ int main(void)
 
 	for (i = 1; i < 100; i++)
 	{
-		if ((i % 3 == 0) | (i % 5 == 0))
-			printf("FizzBuzz");
-		if ((i % 5 == 0) | (i % 3 != 0))
-			printf ("Buzz");
-		if ((i % 3 == 0) | (i % 5 != 0))
-			printf("Fizz");
-		if (i == 100)
-			continue;
-		putchar(' ');
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		if ((i % 5 == 0) && (i % 3 != 0))
+			printf ("Buzz ");
+		if ((i % 3 == 0) && (i % 5 != 0))
+			printf("Fizz ");
+		if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d ", i);
+		if (i == 100 && (i % 5 == 0))
+			printf("Buzz ");
 	}
+	printf('\n');
 	return (0);
 }
