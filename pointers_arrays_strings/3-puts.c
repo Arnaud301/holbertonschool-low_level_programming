7849731a3e1850635f9f80b094a56fcc8a37a352
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-27 14:24:37 +0100

    3Th task

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{	
+		if (str[i] = '\0')
+			continue;
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
