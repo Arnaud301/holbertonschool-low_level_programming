03f63da012212ce2014a8d8deab3f1e26a641288
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-28 09:20:41 +0100

    4th task

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_rev - function
+ *@s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+		i++;
+	i--;
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
