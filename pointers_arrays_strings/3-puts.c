4bdec5bcb7d35f08e7e445597dbf79f50a8aae31
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-28 08:36:34 +0100

    3Th task~

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include "stdio.h"
 /**
- *
- *
- *
+ *_puts - function
+ *@str: string
  */
 void _puts(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
-	{	
+	{
 		_putchar(str[i]);
 		i++;
 	}
