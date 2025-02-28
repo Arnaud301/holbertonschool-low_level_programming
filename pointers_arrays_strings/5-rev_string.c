3ff550901775131dfe4e6044e45e0f53f0e2c00b
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-28 13:52:56 +0100

    5th task

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,22 @@
 #include "main.h"
 /**
- *
- *
+ *_strlen - function
+ *@s: string
+ *Return: lenght
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        if (s[i] == '\0')
+                return (0);
+        while (s[i] != '\0')
+                i++;
+        return (i);
+}
+/**
+ *rev_string - function 2
+ *@s: string
  */
 void rev_string(char *s)
 {
