b342d2e8f523bf2ae5d6820e3ae15da4ae50484e
Arnaud301 <arnaud2bonnet@gmail.com>
2025-02-28 10:53:13 +0100

    5th task

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,15 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	int a = 0;
 	char string[] = "";
 
 	while (s[i] != '\0')
 		i++;
 	while (s[i] > 0)
 	{
-		s[i] = string[i];
+		s[i] = string[a];
 		i--;
+		a++;
 	}
 }
