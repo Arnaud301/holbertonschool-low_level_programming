c31c24a3a731d485c4b40625c9a77327d668d5be
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-03 13:49:33 +0100

    2th task

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	for (; s != c; s++)
+	{
+		if (s == c)
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+		return (s);
+	return (s);
+}
