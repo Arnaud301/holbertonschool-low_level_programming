1afda686ac77312ae244f6175a1ef8aba8970568
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-03 09:42:33 +0100

    0th task

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		if (s[i] != '\0')
+			i++;
+		else
+			break;
+	}
+	return (s);
+}
