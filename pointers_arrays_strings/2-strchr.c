855853c63557b48e1a1bde32f8c0a921b970db52
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-03 13:51:54 +0100

    2th task

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ *_strchr - function that find the corresponding caracter in a string
+ *@s: the string
+ *@c: the caracter
+ *Return: when the caracter is found we return
  */
 char *_strchr(char *s, char c)
 {
