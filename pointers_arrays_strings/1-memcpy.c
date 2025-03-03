c3a28394b20a2c8beba12aab93f5f7accb972522
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-03 10:03:21 +0100

    1th task

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memcpy - function
+ *@dest: char
+ *@src: char
+ *@n: unsigned int
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+	return (dest);
+}
