b8d360d3854134c10c892cda3e46a978bad76735
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-04 13:40:26 +0100

    5th task

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	if (needle[i] == '\0')
+		return (haystack);
+
+	while (haystack[i] != needle[j])
+	{
+		if (haystack[i] == needle[j])
+		{
+			j++;
+		}
+		i++;
+	}
+	return (s + j);
+}
