74b0c73b1d8f50bb75cde0896a4174c2f553447e
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-07 15:44:06 +0100

    3th task

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int factorial(int n)
+{
+	if (n >= 1)
+	{
+		return (factorial * n(n + 1));
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+		return (-1);
+}
