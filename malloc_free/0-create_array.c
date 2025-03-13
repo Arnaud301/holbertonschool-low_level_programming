883da8c7d16792e910e1ec00d85131f1553be677
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-13 09:30:24 +0100

    0th task

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (array);
+	for (; i < size; i++;)
+		array[i] = c;
+	return (array);
+}
