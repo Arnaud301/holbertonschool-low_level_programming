75bac42aad5e61babdced98753e4010b414cacbf
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-13 14:05:09 +0100

    1th task

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strdup - function
+ *@str: string
+ *Return: the newly allocated string wich is a copy of str
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	array = malloc(len + 1);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		array[i] = str[i];
+
+	return (array);
+}
