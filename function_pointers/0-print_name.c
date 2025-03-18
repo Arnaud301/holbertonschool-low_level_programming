8adee21d437e22dc28e0ceaa886e8f1bce7db238
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-18 10:06:51 +0100

    0th task

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ *print_name - function
+ *@f: function pointer
+ *@name: the name u got to print
  */
 void print_name(char *name, void (*f)(char *))
 {
