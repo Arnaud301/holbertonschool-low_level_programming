5da3ee2ec4da64407a682480f1f5249020fdfbaa
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-20 11:48:32 +0100

    0th task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,14 +8,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
+	int sum = 0;
 	va_list args;
-	va_start(args, n);
 
 	if (n == 0)
 		return (0);
-	while (i < n)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		i++;
+		sum += va_arg(args, int);
 	}
-	return (i);
+	va_end(args);
+	return (sum);
 }
