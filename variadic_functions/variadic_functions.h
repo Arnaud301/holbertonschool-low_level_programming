8a7c355d9756a8e8b9a1342ef504ffb3ccf2abaa
Arnaud301 <arnaud2bonnet@gmail.com>
2025-03-25 11:24:09 +0100

    tasks

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,4 @@
-#define VARIADIC_FUNCTIONS_H
+
 
 /*
  *  * File: variadic_functions.h
@@ -25,5 +25,3 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif
